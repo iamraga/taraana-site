3f6b1f86ee3c0e65e7a7e1375825d862245d5e2c
Ragavendhar <imraga@gmail.com>
2022-04-01 14:31:14 +0530

    Image compression on upload

--- comps/admin/uploadForm.js
+++ comps/admin/uploadForm.js
@@ -4,6 +4,7 @@ import { PlusOutlined } from '@ant-design/icons';
 import { collection, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 import { storage, firestore, serverTimestamp, increment } from '../../utils/firebase';
 import { ref, uploadBytesResumable, deleteObject, getDownloadURL } from 'firebase/storage';
+import Compressor from 'compressorjs';
 
 function getBase64(file) {
     return new Promise((resolve, reject) => {
@@ -34,30 +35,38 @@ export default function UploadForm({ albumId }) {
         setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
     };
 
-    function uploadImages({ file: uploadedFile, onSuccess, onProgress, onError }) {
+    async function uploadImages({ file: uploadedFile, onSuccess, onProgress, onError }) {
         const imageNameTimestamp = Date.now();
-        const storageRef = ref(storage, 'images/' + (uploadedFile.name + "##" + imageNameTimestamp)); //Adding timestamp to prevent duplicates
-        const imagesCollRef = collection(firestore, `albums/${albumId}/images`);
-        const albumDocRef = doc(firestore, `albums/${albumId}`);
-        
-        const uploadTask = uploadBytesResumable(storageRef, uploadedFile); 
-        uploadTask.on('state_changed', (snap) => {
-                let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-                onProgress({progress: percentage});
-            }, (err) => {
-                onError(err);
-            }, async () => { //Fired after complete upload
-                const url = await getDownloadURL(storageRef);
+
+        //compressing images before upload
+        new Compressor(uploadedFile, {      
+            quality: 0.65,
+            success: (resizedFile) => {
+                console.log("inside")
+                const storageRef = ref(storage, 'images/' + (resizedFile.name + "##" + imageNameTimestamp)); //Adding timestamp to prevent duplicates
+                const imagesCollRef = collection(firestore, `albums/${albumId}/images`);
+                const albumDocRef = doc(firestore, `albums/${albumId}`);
                 
-                //Entry in Firestore in current Album
-                addDoc(imagesCollRef, { name: uploadedFile.name, imageNameinStorage: (uploadedFile.name + "##" + imageNameTimestamp), url, createdAt: serverTimestamp() })
-                    .then(function(newDocRef){
-                        onSuccess({documentId: newDocRef.id, storageName: (uploadedFile.name + "##" + imageNameTimestamp), url, status: "ok"});
-                    })
-                    .catch(err => onError(err));
-                //Increment collection count value
-                updateDoc(albumDocRef, { imageCount : increment(1)});
-            });
+                const uploadTask = uploadBytesResumable(storageRef, resizedFile); 
+                uploadTask.on('state_changed', (snap) => {
+                        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+                        onProgress({progress: percentage});
+                    }, (err) => {
+                        onError(err);
+                    }, async () => { //Fired after complete upload
+                        const url = await getDownloadURL(storageRef);
+                        
+                        //Entry in Firestore in current Album
+                        addDoc(imagesCollRef, { name: resizedFile.name, imageNameinStorage: (resizedFile.name + "##" + imageNameTimestamp), url, createdAt: serverTimestamp() })
+                            .then(function(newDocRef){
+                                onSuccess({documentId: newDocRef.id, storageName: (resizedFile.name + "##" + imageNameTimestamp), url, status: "ok"});
+                            })
+                            .catch(err => onError(err));
+                        //Increment collection count value
+                        updateDoc(albumDocRef, { imageCount : increment(1)});
+                    });
+            },
+        });
     }
 
     function handleRemove(file) {
